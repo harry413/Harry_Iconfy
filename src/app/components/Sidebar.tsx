f1d237fd25270d1c6ec5fd37977664b9db0f8654
sachin413 <sachinpatidar413@gmail.com>
2024-08-08 16:47:35 +0530

    sidebar changes

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -30,10 +30,10 @@ export default function Sidebar() {
                   Get started
                 </a>
               </li>
-              {/* <li><a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-300 rounded-lg hover:bg-gray-600" href="#">
+              <li><a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-300 rounded-lg hover:bg-gray-600" href="#">
                 <svg className="size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="4" rx="2" ry="2"/><line x1="16" x2="16" y1="2" y2="6"/><line x1="8" x2="8" y1="2" y2="6"/><line x1="3" x2="21" y1="10" y2="10"/><path d="M8 14h.01"/><path d="M12 14h.01"/><path d="M16 14h.01"/><path d="M8 18h.01"/><path d="M12 18h.01"/><path d="M16 18h.01"/></svg>
-                Calendar
-              </a></li> */}
+                NPM
+              </a></li>
               <li><a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-300 rounded-lg hover:bg-gray-600" href="#">
                 <svg className="size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
                 Documentation
